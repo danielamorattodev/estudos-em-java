d6cf78b7e826264f59e7beef04942537d3452d51
Daniela-ddlm <danieladelmondes191@gmail.com>
2022-07-12 18:09:38 -0300

    Exercicio de método Java 1

--- src/dio/com/br/metodos/Main.java
+++ src/dio/com/br/metodos/Main.java
@@ -10,5 +10,18 @@ public class Main {
         exercicioCalculadora.subtracao(9, 1.8);
         exercicioCalculadora.multiplicacao(7,8);
         exercicioCalculadora.divisao(5,2.5);
+
+        //Mensagem
+        System.out.println("exercicio Mensagem");
+        exercicioMensagem.obterMensagem(9);
+        exercicioMensagem.obterMensagem(14);
+        exercicioMensagem.obterMensagem(1);
+
+        //Emprestimo
+        System.out.println("exercicio de Empréstimo");
+        exercicioEmprestimo.calcular(1000, exercicioEmprestimo.getDuasParcelas());
+        exercicioEmprestimo.calcular(1000, exercicioEmprestimo.getTresParcelas()); // exemplos de parametros como metodo;
+        exercicioEmprestimo.calcular(1000, 5);
+
     }
 }
--- src/dio/com/br/metodos/exercicioCalculadora.java
+++ src/dio/com/br/metodos/exercicioCalculadora.java
@@ -1,10 +1,11 @@
 package dio.com.br.metodos;
 
+//exercicio de uso de metodos simples;
 public class exercicioCalculadora {
 
     public static void soma(double numero1, double numero2) {
         double resultado = numero1 + numero2;
-        System.out.println("A soma de " + numero1 + "mais" + numero2 + " é " + resultado);
+        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é " + resultado);
 
     }
 
@@ -16,13 +17,13 @@ public class exercicioCalculadora {
 
     public static void multiplicacao(double numero1 , double numero2) {
         double resultado = numero1 * numero2;
-        System.out.println("A multiplicação de " + numero1 + "vezes" + numero2 + " é " + resultado);
+        System.out.println("A multiplicação de " + numero1 + " vezes " + numero2 + " é " + resultado);
 
     }
 
     public static void divisao(double numero1 , double numero2) {
         double resultado = numero1 / numero2;
-        System.out.println("A divisão de " + numero1 + "por" + numero2 + " é " + resultado);
+        System.out.println("A divisão de " + numero1 + " por " + numero2 + " é " + resultado);
 
     }
 
