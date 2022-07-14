320410230dee96bdda11d541eadfb04b3c4746bd
Daniela-ddlm <danieladelmondes191@gmail.com>
2022-07-14 12:48:35 -0300

    Estudo de loops em JAVA

--- src/repeticoes_e_arrays/Loops/Ex6_Fatorial.java
+++ src/repeticoes_e_arrays/Loops/Ex6_Fatorial.java
@@ -1,4 +1,27 @@
 package repeticoes_e_arrays.Loops;
 
+import java.util.Scanner;
+
+/*
+Faça um programa que calcule o fatorial de um número inteiro fornecido pelo usuário.
+Ex.: 5!=5.4.3.2.1=120
+*/
 public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); //Abertura da entrada de fluxo de dados através do teclado
+
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+
+        int multiplicacao = 1; //variável para guardar o valor da multiplicação dos valores.
+
+        System.out.println(fatorial +"! = ");  //para: variável i = numero, até i maior que 0, diminuimos 1 ao valor de i a cada loop.
+        for( int i = fatorial; i >= 1; i--){
+            multiplicacao *= i; //multiplicamos os valores de i e armazenamos na variável multiplicação
+        }
+
+        //imprima o valor resultado da multiplicação
+        System.out.println(multiplicacao);
+    }
 }
